2b7ce1c2d5c8f1e7835d92b9fbf7764431a24f59
Jelika <smirnova.lika.23092001@gmail.com>
2019-02-23 02:32:38 +0300

    feat: love triangles countets

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(a = []) {
+	// your implementation
+	let count = 0;
+	for (i = 0; i < a.length; i++) {
+		if ((i === a[ a[ a[i] - 1] - 1] - 1) && (a[i] !== (i + 1)) ) { 
+			console.log(a[a[ a[i] - 1] - 1] - 1, i);
+			a[i]= 10000;
+			count++;
+		}
+  }
+return count;
 };
